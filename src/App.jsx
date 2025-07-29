7183a4e2ff4adfa2da31ded1831374fccd1fc18d
Herman-Ihnatenko <german1514u@gmail.com>
2025-07-29 12:48:58 +0200

    HomeWork_02

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 // Lessons
-import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
+//  import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
 
 // Homeworks
-
+import Homework02 from "./homeworks/HomeWork_02/HomeWork_02";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       {/* Lessons */}
-      <Lesson_02 />
+      {/* <Lesson_02 /> */}
       {/* Homeworks */}
+      <Homework02 />
     </div>
   );
 }
